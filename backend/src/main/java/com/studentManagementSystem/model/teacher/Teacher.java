6e2c738321aa1c09be9dba60ba95511e9a94aae0
Piyawardhana <mdvppiyawardhana@gmail.com>
2023-09-21 10:51:45 +0530

    teacher entity created

---
+++ backend/src/main/java/com/studentManagementSystem/model/teacher/Teacher.java
@@ -0,0 +1,48 @@
+package com.studentManagementSystem.model.teacher;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "teachers", uniqueConstraints = @UniqueConstraint(columnNames = {}))
+public class Teacher {
+
+    @Id
+    @GeneratedValue(generator = "teacher_generator")
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    private Long age;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String phoneNo;
+
+    @NotBlank
+    private String designation;
+
+    @NotNull
+    private Long subjectId;
+    
+}
+
