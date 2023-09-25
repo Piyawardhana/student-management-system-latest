786efdfbd9e109705364688da160742598631d8d
MaheshBawantha <maheshbawantha1998@gmail.com>
2023-09-25 16:03:30 +0530

    comment the test file

--- backend/src/test/java/com/studentManagementSystem/StudentManagementSystemApplicationTests.java
+++ backend/src/test/java/com/studentManagementSystem/StudentManagementSystemApplicationTests.java
@@ -1,13 +1,13 @@
 package com.studentManagementSystem;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StudentManagementSystemApplicationTests {
+// @SpringBootTest
+// class StudentManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
