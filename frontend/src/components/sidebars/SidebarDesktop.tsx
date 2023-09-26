bc4fab6c45f444b200e825c4b3031a1a8ce5ec9d
MaheshBawantha <maheshbawantha1998@gmail.com>
2023-09-26 20:22:45 +0530

    add todo list

---
+++ frontend/src/components/sidebars/SidebarDesktop.tsx
@@ -0,0 +1,9 @@
+/**
+ * should implement,
+ * menuItem
+ * menuLinkItem
+ * menuItemContainer
+ * as functions and use menuItem in the sidebar comp.
+ */
+export default function SidebarDesktop() {
+}
